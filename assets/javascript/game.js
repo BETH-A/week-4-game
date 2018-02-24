093bc53afd4c23fd20047b1a7a8933d01e244d44
BETH-A <elizabeth78@me.com>
2018-02-23 21:49:19 -0500

    week-4-game

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+
+//======================================
+// GLOBAL VARIABLES
+//======================================
+
+
+// Character Objects:
+    // Each character has 3 attributes: Health Points, Attack Power and Counter Attack Power
+        // The Health Points, Attack Power and Counter Attack Power of each character must differ
+
+var fighter1 = {name: "fighter1Name", healthPoint: 100, attackPower: 10, counterAttack: 10};
+
+var fighter2 = {name: "fighter2Name", healthPoint: 150, attackPower: 5, counterAttack: 5};
+
+var fighter3 = {name: "fighter3Name", healthPoint: 135, attackPower: 15, counterAttack: 15};
+
+var fighter4 = {name: "fighter4Name", healthPoint: 195, attackPower: 25, counterAttack: 25};
+
+// Player & Defender Names
+
+var playerName = "";
+
+var defenderName = "";
+
+
+
+//========================================
+// FUNCTIONS
+//========================================
+
+// Each time the player attacks, their character's Attack Power increases by its base Attack Power
+
+// The enemy character only has Counter Attack Power 
+    // Unlike the player's Attack Points, Counter Attack Power never changes
+
+
+
