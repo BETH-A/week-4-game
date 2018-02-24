a62a307c072e45a00f7bab692d7fc5732fd6edf5
BETH-A <elizabeth78@me.com>
2018-02-23 22:05:25 -0500

    week-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,8 @@
     // Each character has 3 attributes: Health Points, Attack Power and Counter Attack Power
         // The Health Points, Attack Power and Counter Attack Power of each character must differ
 
+var fighters = ["fighter1", "fighter2", "fighter3", "fighter4"];
+
 var fighter1 = {name: "fighter1Name", healthPoint: 100, attackPower: 10, counterAttack: 10};
 
 var fighter2 = {name: "fighter2Name", healthPoint: 150, attackPower: 5, counterAttack: 5};
@@ -28,6 +30,31 @@ var defenderName = "";
 // FUNCTIONS
 //========================================
 
+// console log for testing
+
+for (var i = 0; i < fighters.length; i++) {
+function consoleLogVariables() {
+
+    console.log("fighter1: " + fighter1);
+
+    console.log("fighter2: " + fighter2);
+
+    console.log("fighter3: " + fighter3);
+
+    console.log("fighter4: " + fighter4);
+
+    // console.log("isGameOver: " + isGameOver + " isGameBegun: " + isGameBegun);
+
+    // console.log("playerName: " + playerName + " defenderName: " + defenderName);
+
+    // console.log("gamePausedForAnotherDefender: " + gamePausedForAnotherDefender);
+
+    console.log("============================================")
+
+}
+
+consoleLogVariables();
+}
 // Each time the player attacks, their character's Attack Power increases by its base Attack Power
 
 // The enemy character only has Counter Attack Power 
